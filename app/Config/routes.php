aaaa425b37c48a199dbf3be1e80f1178ef0fed3b
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-01-04 12:06:38 -0500

    logi basico

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -2,9 +2,11 @@
 
 // views
 $f3->route('GET /', '\App\Controllers\HomeController->index');
+// login
 $f3->route('GET /login', '\App\Controllers\LoginController->index');
 $f3->route('GET /login/sign-in', '\App\Controllers\LoginController->index');
 $f3->route('GET /login/reset-password', '\App\Controllers\LoginController->index');
+$f3->route('POST /login', '\App\Controllers\LoginController->access');
 // rest
 $f3->route('GET /specialism/list', '\App\Controllers\SpecialismController->list');
 // handler
--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -32,4 +32,25 @@ class LoginController extends BaseController
     http_response_code(200);
     echo $this->render('login/index', $locals);
   }
+
+  function access($f3, $args)
+  {
+    $payload = $f3->get('POST');
+    $user = $payload['user'];
+    $password = $payload['password'];
+    if($user == 'admin' && $password == '123'){
+      $f3->set('SESSION.csrfKey', \App\Libraries\RandomLib::stringNumber(20));
+      $f3->set('SESSION.csrfValue', \App\Libraries\RandomLib::stringNumber(30));
+      $f3->set('SESSION.status', 'active');
+      $f3->set('SESSION.user', $user);
+      $f3->set('SESSION.name', 'Pepe Valdivia');
+      $f3->set('SESSION.img', $f3->get('staticURL') . 'assets/img/default-user.png');
+      $f3->set('SESSION.time', date('Y-m-d H:i:s'));
+      header('Location: ' . '/');
+      exit();
+    }else{
+      $f3->reroute($f3->get('PATH') . '?error=user-pass-mismatch');
+      exit();
+    }
+  }
 }
