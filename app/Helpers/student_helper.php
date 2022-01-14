07b6f6a33a60f5b545f23b17b14784658fb901b7
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2022-01-14 17:38:44 -0500

    genrarndo qrs en cursos

--- app/Helpers/student_helper.php
+++ app/Helpers/student_helper.php
@@ -47,5 +47,23 @@ function sendPDF($student, $folder, $baseFile, $type, $event_id, $webURL)
     $page->setFont($customFont, 36);
     $page->drawText($student->{'grade'}, 625, 180);
   }
+  if($type == 'course'){ // diplomado
+    // qr
+    $page = $pdf->pages[0];
+    $page->drawImage($image, 90, 35, 265, 210); // source, x1, y1, x2, y2 || 150, 300, 350, 500
+    $pdf->pages[0] = $page;
+    $page->setFont($customFont, 22);
+    // name
+    $textWidth = widthForStringUsingFontSize(
+      $student->{'last_names'} . ' ' . $student->{'first_names'}, $customFont, 22
+    );
+    $position = (800 - $textWidth)/2;
+    $page->drawText(strtoupper($student->{'last_names'} . ' ' . $student->{'first_names'}), $position, 330);
+    // code
+    $page = $pdf->pages[1];
+    $page->setFont($customFont, 26);
+    $page->drawText($student->{'code'}, 625, 330);
+  }
+  
   $pdf->save($folder . $student->{'id'} . ' ' . $student->{'last_names'} . ' ' . $student->{'first_names'});
 }
